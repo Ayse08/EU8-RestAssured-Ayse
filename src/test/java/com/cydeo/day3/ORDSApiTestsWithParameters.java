817607d832ee00e66d156d3ed94c3e4addc65a62
Ayse <aysenur_aydin@hotmail.fr>
2022-07-19 09:22:08 +0200

    day4 ORDSPath with class done

--- src/test/java/com/cydeo/day3/ORDSApiTestsWithParameters.java
+++ src/test/java/com/cydeo/day3/ORDSApiTestsWithParameters.java
@@ -60,7 +60,6 @@ public class ORDSApiTestsWithParameters {
 
         assertEquals(200, response.statusCode() );
         assertEquals("application/json", response.header("Content-Type"));
-
         assertTrue(response.body().asString().contains("IT_PROG"));
 
         response.prettyPrint();
