25abe3c6a8b3d177328b1538d745e8d36e590b2f
Ayse <aysenur_aydin@hotmail.fr>
2022-07-25 12:19:38 +0200

    day8 new SpartanAuthTestBase and SpartanWithAuthTests .java admin test is done

--- src/test/java/com/cydeo/day8/SpartanWithAuthTests.java
+++ src/test/java/com/cydeo/day8/SpartanWithAuthTests.java
@@ -1,7 +1,41 @@
 package com.cydeo.day8;
 
-public class SpartanWithAuthTests {
+import com.cydeo.utilities.SpartanAuthTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class SpartanWithAuthTests extends SpartanAuthTestBase {
+
+@DisplayName("GET /api/spartans as a public user(guest) expect 401")
+    @Test
+    public void test1(){
+
+    given().accept(ContentType.JSON).
+            when().
+            get("/api/spartans")
+            .then().statusCode(401)
+            .log().all();
+}
+
+@DisplayName("GET /api/spartans as admin user expect 200")
+    @Test
+    public void testAdmin(){
+    //how to pass admin admin as username and password?
+    given()
+            .auth().basic("admin", "admin")
+            .and().accept(ContentType.JSON)
+            .when()
+            .get("/api/spartans")
+            .then()
+            .statusCode(200)
+            .log().all();
 
 
+}
 
 }
